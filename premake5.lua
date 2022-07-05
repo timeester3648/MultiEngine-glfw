a5ef5d587fc84e42bf4e60e54dc1fbd2f1fb8722
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:35:23 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,65 @@
+include "../../premake/common_premake_defines.lua"
+
+project "glfw"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.h",
+		"./**.c",
+		"./**.m"
+	}
+
+	includedirs {
+		"%{prj.name}/include",
+
+		"%{IncludeDir.glfw}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.vulkan_sdk}",
+		"%{IncludeDir.general_includes}"
+	}
+
+ 	filter "system:windows"
+		defines { "_GLFW_WIN32", "_CRT_SECURE_NO_WARNINGS" }
+		excludes {
+			"./src/wl_init.c",
+			"./src/x11_init.c",
+			"./src/null_init.c",
+			"./src/wl_window.c",
+			"./src/x11_window.c",
+			"./src/wl_monitor.c",
+			"./src/cocoa_time.c",
+			"./src/posix_time.c",
+			"./src/x11_monitor.c",
+			"./src/glx_context.c",
+			"./src/null_window.c",
+			"./src/null_monitor.c",
+			"./src/null_joystick.c",
+			"./src/posix_thread.c",
+			"./src/linux_joystick.c",
+
+			"./**.m"
+		}
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
